b1522be11306b5acde9df44c7a9721cae7cc5e5b
ErOpg-oss <kutcepaloverop@gmail.com>
2025-09-19 08:12:50 +0300

    Изменения

--- Задачи_ОАиП_семинар_2/Задача_8.cpp
+++ Задачи_ОАиП_семинар_2/Задача_8.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <cctype>
-#include <clocale>
 
+bool isVowel(char c) {
+    switch (tolower(c)) {
+        case 'a': case 'e': case 'i': case 'u': case 'o': case 'y':
+            return true;
+        default:
+            return false;
+    }
+}
 
 int main() {
-    setlocale(LC_ALL, "Russian");
-    char c, temp;
-    std::cin>>c;
-    c = tolower(c);
-    std::cout<<с<<std::endl;
-    if (c == 'а' || c == 'е' || c == 'ё' || c == 'и' || c == 'о' || c == 'у' || c == 'ы' || c == 'э' || c == 'ю' || c == 'я') 
-    {
-            std::cout<<"Гласная"<<std::endl;
+    char input;
+    std::cout << "Введите символ: ";
+    std::cin >> input;
+
+    if (isVowel(input)) {
+        std::cout << "'" << input << "' — гласная буква." << std::endl;
+    } else {
+        std::cout << "'" << input << "' — не гласная буква." << std::endl;
     }
-    else
-    {
-        std::cout<<"Согласная";
-    }
-    return 0; 
+
+    return 0;
 }
